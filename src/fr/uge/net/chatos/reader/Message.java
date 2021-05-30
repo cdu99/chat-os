8f1e6edc28331f64cf86cf3846019e7ae558f725
Calvin Du <calvin.du@vidal.fr>
2021-05-30 13:59:32 +0200

    remove useless shit in message

--- src/fr/uge/net/chatos/reader/Message.java
+++ src/fr/uge/net/chatos/reader/Message.java
@@ -1,17 +1,12 @@
 package fr.uge.net.chatos.reader;
 
-// TODO c'est dla merde
-// Remove opcode and connectId
 public class Message {
    private final String pseudo;
    private final String msg;
-   private int opcode;
-   private long connectId;
 
    public Message(String pseudo, String message) {
       this.pseudo = pseudo;
       this.msg = message;
-      connectId = -1;
    }
 
    @Override
@@ -22,27 +17,11 @@ public class Message {
             '}';
    }
 
-   public int getOpcode() {
-      return opcode;
-   }
-
-   public void setOpcode(int opcode) {
-      this.opcode = opcode;
-   }
-
    public String getPseudo() {
       return pseudo;
    }
 
    public String getMsg() {
       return msg;
-   }
-
-   public void setConnectId(long id) {
-      this.connectId = id;
-   }
-
-   public long getConnectId() {
-      return connectId;
    }
 }
